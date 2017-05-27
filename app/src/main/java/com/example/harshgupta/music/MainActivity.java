ccafc30a0006222226799c9d814f5f0495e2e877
Harsh Gupta <hrshgpt22@gmail.com>
2017-05-27 23:54:36 +0530

    first music app

--- app/src/main/java/com/example/harshgupta/music/MainActivity.java
+++ app/src/main/java/com/example/harshgupta/music/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.harshgupta.music;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button play,pause,stop;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        play=(Button)findViewById(R.id.play);
+        pause=(Button)findViewById(R.id.pause);
+        stop=(Button)findViewById(R.id.stop);
+
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp=MediaPlayer.create(MainActivity.this,R.raw.abcd);
+                mp.start();
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mp.stop();
+                MainActivity.this.finish();
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mp.isPlaying()){
+                    mp.pause();
+                } else {
+                    mp.start();
+                }
+
+            }
+        });
     }
 }
